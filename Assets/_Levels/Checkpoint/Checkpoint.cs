8d97368c4c8a272759ca49e2509f7ff5078653e1
Tomáš Ježek <tomas.jezek1@seznam.cz>
2018-02-26 00:34:26 +0100

    Folder reorganisation

---
+++ Assets/_Levels/Checkpoint/Checkpoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Checkpoint : MonoBehaviour {
+    public Transform level;
+
+    Inventory inventory;
+    LevelManager levelManager;
+    List<IRestartable> restartables = new List<IRestartable>();
+    List<InventoryItem> inventoryState = new List<InventoryItem>();
+
+    void Awake() {
+        inventory = FindObjectOfType<Inventory>();
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            levelManager.CheckpointReached(this);
+            SaveState();
+        }
+    }
+
+    void SaveState() {
+        restartables.Clear();
+        restartables.AddRange(level.GetComponentsInChildren<IRestartable>());
+
+        inventoryState = inventory.Items;
+    }
+
+    public void RestoreState() {
+        inventory.Items = inventoryState;
+
+        foreach (IRestartable restartable in restartables) {
+            restartable.Restart();
+        }
+    }
+}
---
+++ Assets/_Levels/Exit/EndGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Finish");
+        }
+    }
+}
