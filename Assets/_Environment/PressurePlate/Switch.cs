6163d24884c2e285621ab4ad0b35bf020f23eaf4
Unknown <oliver.pacut@gmail.com>
2018-04-06 21:39:51 +0200

    Spiky level added

---
+++ Assets/_Environment/PressurePlate/Switch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public bool On;
+
+    public List<SpikeTrap> Spikes;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        On = !On;
+        Spikes.ForEach(y => y.Toggle());
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
