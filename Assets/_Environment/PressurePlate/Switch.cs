2f03cec2b5415fe8124189bd53424b7d18bc4497
Maroš Beťko <betkomaros@gmail.com>
2018-04-08 16:45:51 +0200

    Cleaned up prefabs, added torn ladder tile and switch prefab.

--- Assets/_Environment/PressurePlate/Switch.cs
+++ Assets/_Environment/PressurePlate/Switch.cs
@@ -2,24 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Switch : MonoBehaviour
-{
+public class Switch : MonoBehaviour {
     public bool On;
-
     public List<SpikeTrap> Spikes;
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
+    private void OnTriggerEnter2D(Collider2D other) {
         On = !On;
         Spikes.ForEach(y => y.Toggle());
     }
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 }
