7130ca6e9b0b2a02db4c61916e95bc58065182a2
Maroš Beťko <betkomaros@gmail.com>
2018-05-14 16:50:20 +0200

    Clouds and ship clutter.

---
+++ Assets/_Environment/Clouds/Cloud.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Cloud : MonoBehaviour {
+	private SpriteRenderer renderer;
+	private Rigidbody2D rbody;
+	public Sprite cloud {
+		get { return renderer.sprite; }
+		set { renderer.sprite = value; }
+	}
+	public float endX = 0;
+	public float speed {
+		set {
+			rbody.velocity = new Vector2(-value, 0);
+		}
+	}
+
+	// Use this for initialization
+	void Start() {
+		renderer = GetComponent<SpriteRenderer>();
+		rbody = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (transform.position.x < endX) {
+			Destroy(gameObject);
+		}
+	}
+}
