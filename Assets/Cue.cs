8c7ff611911c1d1c3560910220871da357ef1b79
Unknown <oliver.pacut@gmail.com>
2018-05-15 00:38:21 +0200

    Sound effects, tutorial cues

---
+++ Assets/Cue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cue : MonoBehaviour
+{
+    [SerializeField]
+    Text tutorialText;
+
+    [SerializeField]
+    string updateText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            tutorialText.text = updateText;
+        }    
+    }
+}
