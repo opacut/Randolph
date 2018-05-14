4c73a7620a706672612119b0667b8ac7a2962010
Maroš Beťko <betkomaros@gmail.com>
2018-05-14 20:23:23 +0200

    Clooooouds

--- Assets/_Environment/Clouds/Cloud.cs
+++ Assets/_Environment/Clouds/Cloud.cs
@@ -2,32 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
-[RequireComponent(typeof(Rigidbody2D))]
-public class Cloud : MonoBehaviour {
-	private SpriteRenderer renderer;
-	private Rigidbody2D rbody;
-	public Sprite cloud {
-		get { return renderer.sprite; }
-		set { renderer.sprite = value; }
-	}
-	public float endX = 0;
-	public float speed {
-		set {
-			rbody.velocity = new Vector2(-value, 0);
+namespace Randolph.Environment {
+	[RequireComponent(typeof(SpriteRenderer))]
+	[RequireComponent(typeof(Rigidbody2D))]
+	public class Cloud : MonoBehaviour {
+		public SpriteRenderer spriteRenderer { get; private set; }
+		public Rigidbody2D rbody { get; private set; }
+
+		public Sprite sprite {
+			get { return spriteRenderer.sprite; }
+			set { spriteRenderer.sprite = value; }
 		}
-	}
+		public Color color { set { spriteRenderer.color = value; } }
+		public float speed { set { rbody.velocity = new Vector2(-value, 0); } }
 
-	// Use this for initialization
-	void Start() {
-		renderer = GetComponent<SpriteRenderer>();
-		rbody = GetComponent<Rigidbody2D>();
-	}
+		public float endX = 0;
 
-	// Update is called once per frame
-	void Update() {
-		if (transform.position.x < endX) {
-			Destroy(gameObject);
+		private void Awake() {
+			spriteRenderer = GetComponent<SpriteRenderer>();
+			rbody = GetComponent<Rigidbody2D>();
+		}
+
+		private void Update() {
+			if (transform.position.x < endX) {
+				Destroy(gameObject);
+			}
 		}
 	}
 }
