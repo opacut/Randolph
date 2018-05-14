b32d5d7cfe668fa3082e3bca63b0f4389fce6602
Unknown <oliver.pacut@gmail.com>
2018-05-14 23:07:11 +0200

    Added speech bubbles

---
+++ Assets/_UI/TypewriterEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypewriterEffect : MonoBehaviour
+{
+    public float delay = 0.1f;
+    public string fullText;
+    private string currentText = "";
+
+    void Start()
+    {
+        GetComponent<Text>().text = "";
+    }
+
+    [ContextMenu("ShowText")]
+    public IEnumerator ShowText()
+    {
+        for(int i = 0; i<fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+	
+}
