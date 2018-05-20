39514ed5b8492fce6d317729e99e0b6dac77f318
Haaxor1689 <betkomaros@gmail.com>
2018-05-20 23:32:10 +0200

    Few new assets

---
+++ Assets/_Environment/Ship/AlphaFade.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Randolph.Environment {
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class AlphaFade : MonoBehaviour {
+        private SpriteRenderer spriteRenderer;
+
+        private void Start() { spriteRenderer = GetComponent<SpriteRenderer>(); }
+
+        // Update is called once per frame
+        private void Update() {
+            var t = Mathf.Sin(Time.time * 2) / 2 + 0.5f;
+            spriteRenderer.color = new Color(1.0f, 1.0f, 1.0f, t);
+        }
+    }
+}
