b79279c87568d685412d0cb5da005705022a3a67
Haaxor1689 <betkomaros@gmail.com>
2018-05-22 00:16:51 +0200

    Hook eye fixes and polishing

---
+++ Assets/_Interactable/Interactable/Hook joint/HookEye.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Randolph.Characters;
+
+namespace Randolph.Interactable {
+    public class HookEye : Interactable {
+        PlayerController player;
+
+        protected override void Awake() {
+            base.Awake();
+            player = FindObjectOfType<PlayerController>();
+        }
+
+        public void Activate() => player.GrappleTo(gameObject);
+
+        public override void OnInteract() => Debug.Log("Clicked on hook eye");
+    }
+}
