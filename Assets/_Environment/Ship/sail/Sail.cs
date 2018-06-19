07673bdd0720e020484427b253c550148db48ddb
Unknown <oliver.pacut@gmail.com>
2018-06-19 11:53:32 +0200

    Sail now restarts properly

--- Assets/_Environment/Ship/sail/Sail.cs
+++ Assets/_Environment/Ship/sail/Sail.cs
@@ -2,16 +2,21 @@
 using Randolph.Interactable;
 using System.Linq;
 using UnityEngine;
+using Randolph.Levels;
 
 namespace Randolph.Environment {
-    public class Sail : MonoBehaviour {
+    public class Sail : MonoBehaviour, IRestartable {
         [SerializeField] private Vector2 destination = new Vector2(-100f, 50f);
         [SerializeField] private float speed = 20f;
         [SerializeField] private TiedRope[] ropes;
         private Animator animator;
 
 
-        private void Start() { animator = GetComponent<Animator>(); }
+        private void Start()
+        {
+            SaveState();
+            animator = GetComponent<Animator>();
+        }
 
         private void Update() {
             if (animator.GetBool("CutOff")) {
@@ -23,9 +28,11 @@ namespace Randolph.Environment {
             var delta = speed * Time.deltaTime;
 
             Vector3 newPosition = Vector2.MoveTowards(transform.position, destination, delta);
+            /*
             if (newPosition == transform.position) {
                 Destroy(gameObject);
             }
+            */
             transform.position = newPosition;
             transform.Rotate(Vector3.forward, speed * delta * delta);
         }
@@ -38,7 +45,28 @@ namespace Randolph.Environment {
                 animator.SetBool("CutOff", true);
             }
         }
-        
+
+        #region IRestartable
+        private bool initialActiveState;
+        protected Vector3 initialPosition { get; private set; }
+        private Quaternion initialRotation;
+
+        public void SaveState()
+        {
+            initialActiveState = gameObject.activeSelf;
+            initialPosition = transform.position;
+            initialRotation = transform.rotation;
+        }
+
+        public void Restart()
+        {
+            animator.SetBool("CutOff", false);
+            gameObject.SetActive(initialActiveState);
+            transform.position = initialPosition;
+            transform.rotation = initialRotation;
+        }
+        #endregion
+
         public event Action OnSlash;
     }
 }
